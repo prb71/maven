17c1e96ec66c15fe5f5d82fbcc0a42b80208d10d
mukulrai13 <93812135+mukulrai13@users.noreply.github.com>
2022-06-29 16:05:01 +0530

    Update FirstTest.java

--- src/test/java/clientbased/FirstTest.java
+++ src/test/java/clientbased/FirstTest.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 public class FirstTest {
     @Test
 	public void test() {
-		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
 		driver.manage().window().maximize();
