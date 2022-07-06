80b1caa17f1f9e47620190418c3e5cc87a59ce82
prb71 <96513118+prb71@users.noreply.github.com>
2022-07-06 14:32:00 +0530

    Update FirstTest.java

--- src/test/java/clientbased/FirstTest.java
+++ src/test/java/clientbased/FirstTest.java
@@ -1,6 +1,8 @@
 package clientbased;
 
 import org.testng.annotations.BeforeGroups;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
 import java.time.Duration;
